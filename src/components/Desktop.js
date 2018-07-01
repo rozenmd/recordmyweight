26a600ba117c63288632b12027ae8132e65793c0
Max Rozen <rozenmd@gmail.com>
2018-07-01 22:09:13 +1000

    Tiny refactor for mobile

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import WeightOverTimeChart from './WeightOverTimeChart'
+import { myData } from '../data'
 
 export default class Desktop extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { data: null }
+  }
+  componentDidMount() {
+    // Your parse code, but not seperated in a function
+    let reformattedData = myData.map(row => {
+      return { weight: +row.weight, date: row.date.split(' ')[0] } //new Date(row.date).getTime() / 1000
+    })
+    this.setState({ data: reformattedData })
+  }
   render() {
-    return (
-      // <div style={{ width: '80%', height: '100%' }}>
-      <WeightOverTimeChart height={600} width={800} />
-      // </div>
-    )
+    const { data } = this.state
+    if (data) {
+      return <WeightOverTimeChart height={600} width={800} data={data} />
+    } else {
+      return <Fragment>Loading!</Fragment>
+    }
   }
 }
--- src/components/Mobile.js
+++ src/components/Mobile.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import WeightOverTimeChart from './WeightOverTimeChart'
+import { myData } from '../data'
 
 export default class Mobile extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { data: null }
+  }
+  componentDidMount() {
+    // Your parse code, but not seperated in a function
+    let reformattedData = myData.map(row => {
+      return { weight: +row.weight, date: row.date.split(' ')[0] } //new Date(row.date).getTime() / 1000
+    })
+    this.setState({ data: reformattedData })
+  }
   render() {
-    return (
-      // <div style={{ width: '80%', height: '100%' }}>
-      <WeightOverTimeChart height={500} width={375} />
-      // </div>
-    )
+    const { data } = this.state
+    if (data) {
+      return <WeightOverTimeChart height={500} width={375} data={data} />
+    } else {
+      return <Fragment>Loading!</Fragment>
+    }
   }
 }
